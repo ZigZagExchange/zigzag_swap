69daac8a08784e99100e06eae05ad7ab5d3fca4c
TrooperCrypto <trooper.crypto7@gmail.com>
2022-12-17 15:08:08 +0100

    make swap button look enabled

--- components/swap/swapButton/SwapButton.tsx
+++ components/swap/swapButton/SwapButton.tsx
@@ -310,7 +310,7 @@ export default function SwapButton({ validationStateBuy, validationStateSell }: 
     <button
       className={styles.container}
       onClick={handleSwapButton}
-      disabled={signer === null || swapMode === SwapMode.Disabled}
+      disabled={signer === null || swapMode === SwapMode.Disabled || !sellAmount}
     >
       {buttonText}
     </button>

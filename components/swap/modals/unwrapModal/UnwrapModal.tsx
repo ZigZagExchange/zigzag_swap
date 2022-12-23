5da7939525c2a000ba4d0c9390f368e2a65dd7e9
0xBombadil <kilimanjarocrypto@protonmail.com>
2022-12-23 04:27:52 +0000

    fixed modals

--- components/swap/modals/unwrapModal/UnwrapModal.tsx
+++ components/swap/modals/unwrapModal/UnwrapModal.tsx
@@ -28,31 +28,33 @@ export default function UnwrapModal({ close }: Props) {
     message = "Token swapped."
   }
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.title}>
-        Swapping {sellTokenInfo.symbol} for {buyTokenInfo.symbol}
-      </div>
-      <hr className={styles.hr} />
-      <div className={styles.transaction_progress_container}>
-        <TransactionProgress status={transactionStatus} failed={transactionError !== null} />
-      </div>
-      <div className={styles.buy_sell_tokens}>
-        <div className={styles.sell_token}>
-          {prettyBalance(utils.formatUnits(sellAmount, sellTokenInfo.decimals))}{" "}
-          <div className={styles.token_symbol}>
-            <Image src={`/tokenIcons/${sellTokenInfo.symbol.toLocaleLowerCase()}.svg`} width="100%" height="100%" layout="intrinsic" />
+  if (buyTokenInfo)
+    return (
+      <div className={styles.container}>
+        <div className={styles.title}>
+          Swapping {sellTokenInfo.symbol} for {buyTokenInfo.symbol}
+        </div>
+        <hr className={styles.hr} />
+        <div className={styles.transaction_progress_container}>
+          <TransactionProgress status={transactionStatus} failed={transactionError !== null} />
+        </div>
+        <div className={styles.buy_sell_tokens}>
+          <div className={styles.sell_token}>
+            {prettyBalance(utils.formatUnits(sellAmount, sellTokenInfo.decimals))}{" "}
+            <div className={styles.token_symbol}>
+              <Image src={`/tokenIcons/${sellTokenInfo.symbol.toLocaleLowerCase()}.svg`} width="100%" height="100%" layout="intrinsic" />
+            </div>
+          </div>
+          <div className={styles.arrow}>{rightArrow}</div>
+          <div className={styles.buy_token}>
+            {prettyBalance(utils.formatUnits(buyAmount, buyTokenInfo.decimals))}
+            <div className={styles.token_symbol}>
+              <Image src={`/tokenIcons/${buyTokenInfo.symbol.toLocaleLowerCase()}.svg`} width="100%" height="100%" layout="intrinsic" />
+            </div>
           </div>
         </div>
-        <div className={styles.arrow}>{rightArrow}</div>
-        <div className={styles.buy_token}>
-          {prettyBalance(utils.formatUnits(buyAmount, buyTokenInfo.decimals))}
-          <div className={styles.token_symbol}>
-            <Image src={`/tokenIcons/${buyTokenInfo.symbol.toLocaleLowerCase()}.svg`} width="100%" height="100%" layout="intrinsic" />
-          </div>
-        </div>
+        <div className={styles.text}>{errorMessage ? errorMessage : message}</div>
       </div>
-      <div className={styles.text}>{errorMessage ? errorMessage : message}</div>
-    </div>
-  )
+    )
+  else return <div>Error: buyTokenInfo is missing</div>
 }
